5f72a695b9c568b9a67f4175934d73643e13932a
Cristian <95005685+Criss117@users.noreply.github.com>
2025-09-27 13:22:38 -0500

    feat.api: create product

--- apps/api/src/core/products/controllers/products.controller.ts
+++ apps/api/src/core/products/controllers/products.controller.ts
@@ -1,18 +1,23 @@
 import { Permissions } from '@core/auth/decorators/permissions.decorator';
 import {
+  Body,
   Controller,
   Get,
   HttpException,
   InternalServerErrorException,
   Param,
+  Post,
 } from '@nestjs/common';
 import { FindManyProductsUseCase } from '../use-cases/find-many-products.usecase';
 import { HTTPResponse } from 'src/shared/http/response';
+import { CreateProductDto } from '../dtos/create-product.dto';
+import { CreateProductUsecase } from '../use-cases/create-product.usecase';
 
 @Controller('business/:id/products')
 export class ProductsController {
   constructor(
     private readonly findManyProductsUseCase: FindManyProductsUseCase,
+    private readonly createProductUseCase: CreateProductUsecase,
   ) {}
 
   @Get()
@@ -29,6 +34,27 @@ export class ProductsController {
 
       throw new InternalServerErrorException(
         'Algo salió mal al buscar los productos',
+      );
+    }
+  }
+
+  @Post()
+  @Permissions('products:create')
+  public async create(
+    @Param('id') businessId: string,
+    @Body() data: CreateProductDto,
+  ) {
+    try {
+      const res = await this.createProductUseCase.execute(businessId, data);
+
+      return HTTPResponse.ok(res);
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(
+        'Algo salió mal al crear el producto',
       );
     }
   }
