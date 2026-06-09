d53548414da2fa8e91271ea15b5bbc2ce468b5bc
Cris117 <95005685+Criss117@users.noreply.github.com>
2026-06-09 09:37:09 -0500

    feat(iam): define register organization form schema with validation rules

--- packages/client/src/application/iam/forms/organization.form.ts
+++ packages/client/src/application/iam/forms/organization.form.ts
@@ -3,9 +3,63 @@ import { formOptions } from "@tanstack/react-form";
 import { z } from "zod";
 
 import { useORPC } from "@fludge/client/providers/orpc.provider";
-import { registerOrganizationCommand } from "@fludge/api/modules/iam/organizations/application/commands/register-organization.command";
 
-export type RegisterOrganizationSchema = z.infer<typeof registerOrganizationCommand>;
+export const registerOrganizationSchema = z.object({
+  name: z
+    .string({
+      error: "El nombre es requerido",
+    })
+    .min(3, {
+      error: "El nombre es muy corto",
+    })
+    .max(50, {
+      error: "El nombre es muy largo",
+    }),
+  phone: z
+    .string({
+      error: "El teléfono es requerido",
+    })
+    .min(9, {
+      error: "El teléfono es muy corto",
+    })
+    .max(15, {
+      error: "El teléfono es muy largo",
+    }),
+  legalName: z
+    .string({
+      error: "La razón social es requerida",
+    })
+    .min(3, {
+      error: "La razón social es muy corta",
+    })
+    .max(50, {
+      error: "La razón social es muy larga",
+    }),
+  taxId: z
+    .string({
+      error: "El NIT es requerido",
+    })
+    .min(9, {
+      error: "El NIT es muy corto",
+    })
+    .max(15, {
+      error: "El NIT es muy largo",
+    }),
+  address: z
+    .string({
+      error: "La dirección es requerida",
+    })
+    .min(5, {
+      error: "La dirección es muy corta",
+    })
+    .max(50, {
+      error: "La dirección es muy larga",
+    }),
+});
+
+export type RegisterOrganizationSchema = z.infer<
+  typeof registerOrganizationSchema
+>;
 
 type RegisterOrganizationFormParams = {
   onSuccess?: () => void;
@@ -33,13 +87,14 @@ export function useRegisterOrganizationFormOptions({
         address: "",
       } as RegisterOrganizationSchema,
       validators: {
-        onChange: registerOrganizationCommand,
+        onChange: registerOrganizationSchema,
       },
       onSubmit: ({ value, formApi }) => {
         registerOrganizationMutation.mutate(value, {
           onSuccess: async () => {
             await queryClient.invalidateQueries({
-              queryKey: orpc.organizations.queries.findAll.queryOptions().queryKey,
+              queryKey:
+                orpc.organizations.queries.findAll.queryOptions().queryKey,
             });
             formApi.reset();
             onSuccess?.();
